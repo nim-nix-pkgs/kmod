16086acc73f73bf6345dd169c247726846a7928a
hugosenari <hugosenari@gmail.com>
2022-09-03 19:48:41 -0300

    feat(nimpkgs): update lock

--- source/tests/titerate.nim
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import kmod
-
-# The compiler blew up on this once
-test "iterate through loaded modules":
-  let ctx = newContext()
-  for i in ctx.newModuleListFromLoaded:
-    discard i.name
